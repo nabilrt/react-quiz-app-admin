593c1ec2013b06154ce2eb23be8edbfef223ebd0
Abidur Rahman Nabil <nabilrt51@gmail.com>
2024-11-11 19:23:54 +0600

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import {
-    BrowserRouter,
     createBrowserRouter,
     RouterProvider,
-    Routes,
 } from "react-router-dom";
 import HomePage from "./pages/Homepage.tsx";
 import QuizPage from "./pages/Quiz.tsx";
